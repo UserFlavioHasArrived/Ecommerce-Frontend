3e377d1517a10daca520251031a4c4a226382852
Flavio <flavio_lf_veira@estudante.sesisenai.org.br>
2025-03-05 21:30:23 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let nome = "Flavio";
+let idade = 31;
+const cidade = "Criciuma";
+let a = 2;
+let b = 2;
+let soma = a + b;
+//sout
+console.log("Nome:"+ nome);
+console.log("Idade:"+ idade);
+console.log("Soma:"+ (a+b));
+console.log("Soma:"+soma);
+
+let pessoa ={
+    nome: "Flavio",
+    idade: 31,
+    email:"flavio@teste.com",
+    senha:"testando"
+}
+console.log("Nome:"+pessoa.nome);
+console.log("Idade:"+pessoa.idade);
+console.log(pessoa);
